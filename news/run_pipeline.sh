e35b7f6661265b6c68962c822f462f68e3792a3c
fabiogrillo <fabiogrillo123@gmail.com>
2025-01-03 19:22:16 +0100

    Edited scripts

--- news/run_pipeline.sh
+++ news/run_pipeline.sh
@@ -2,14 +2,15 @@
 
 # Esegui news collector
 echo "Collecting news articles..."
-python3 news_api_collector.py
+python3 ./news/news_api_collector.py
 
 # Esegui sentiment analyzer
 echo "Classifying and storing articles..."
-python3 sentiment_analyzer.py
+python3 ./news/sentiment_analyzer.py
 
 # Classify topics
 echo "Classifying topics"
-python3 topic_classifier.py
+python3 ./news/topic_classifier.py
 
 echo "Pipeline completed!"
+
